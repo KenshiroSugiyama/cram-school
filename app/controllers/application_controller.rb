8b3a90771620904f6ac87b73d4a889c66fbfd1c3
hokudai <ken4600223@gmail.com>
2021-05-30 09:42:08 +0900

    change devise to add column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
     def after_sign_in_path_for(resource)
         pages_mypage_path(resource)
+    end
+
+    protected
+    def configure_permitted_parameters
+       added_attrs = [ :name, :email, :password, :password_confirmation, :school, :school_name, :year]
+       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+       devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 end
