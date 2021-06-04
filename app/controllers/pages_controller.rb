19fcba8ffbbf88f2593f83d7f9a9f26349972c56
hokudai <ken4600223@gmail.com>
2021-06-04 20:15:21 +0900

    edit mypage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,8 @@ class PagesController < ApplicationController
 
   def mypage
     @user = User.find(current_user.id)
+    @current_lessons = Lesson.where(user_id: current_user.id).where(payment: true)
+    @lessons = Lesson.where(user_id: current_user.id).where(payment: false)
   end
 
   def membershipfee
